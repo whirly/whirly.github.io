0db3167e6614423f14f3a0f11cdc0211f2f816f1
Stephane Becker <sbecker@methodinthemadness.eu>
2015-03-20 14:51:18 +0100

    - fix du script de message.

--- js/custom.js
+++ js/custom.js
@@ -435,7 +435,7 @@ $(function() {
 	   // firstName = name.split(' ').slice(0, -1).join(' ');
     //      }        
 	 $.ajax({
-                url: "contact_me.php",
+                url: "http://mailer.methodinthemadness.eu/send",
             	type: "POST",
             	data: {first_name: first_name, last_name: last_name, email: email, message: message},
             	cache: false,
@@ -457,7 +457,7 @@ $(function() {
  		 $('#success').html("<div class='alert alert-danger'>");
             	$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             	 .append( "</button>");
-            	$('#success > .alert-danger').append("<strong>Sorry "+firstName+" it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:me@example.com?Subject=Message_Me;>me@example.com</a> ? Sorry for the inconvenience!");
+            	$('#success > .alert-danger').append("<strong>Désolé "+first_name+" il semble que notre serveur mail ne réponds pas...</strong> Pouvez vous écrire directement à contact, methodinthemadness.eu Désolé pour le dérangement !");
  	        $('#success > .alert-danger').append('</div>');
  		//clear all fields
  		$('#contactForm').trigger("reset");
