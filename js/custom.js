4c03838b2e4580b290e65ab679debffb43a951fe
Stephane Becker <sbecker@methodinthemadness.eu>
2015-03-20 15:05:24 +0100

    - traduction du message d'envoi.

--- js/custom.js
+++ js/custom.js
@@ -445,7 +445,7 @@ $(function() {
             	   $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             		.append( "</button>");
             	  $('#success > .alert-success')
-            		.append("<strong>Your message has been sent. </strong>");
+            		.append("<strong>Votre message a bien été envoyé </strong>");
  		  $('#success > .alert-success')
  			.append('</div>');
  						    
